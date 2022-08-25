ded54f8a3582d70c24efcbefcbb0933939d95400
Путря Дмитро Олександрович <D.Putrya@ferrostroy.local>
2022-08-25 16:33:14 +0300

    added streams and studyclasses init

--- SchoolProject/School.cs
+++ SchoolProject/School.cs
@@ -7,28 +7,34 @@ namespace SchoolProject
 {
     class School
     {
-
         public LearningStream[] LearnStreams { get; set;}
 
-        public School(int CountOfStreams)
+        public School()
         {
-            LearnStreamsInit(CountOfStreams);
+            Console.WriteLine("Enter number of streams");
+            int CountOfStreams = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of classes on stream");
+            int CountClassesOnStream = int.Parse(Console.ReadLine());
+            LearnStreamsInit(CountOfStreams, CountClassesOnStream);
+            TeachersInit(CountOfStreams * CountClassesOnStream);
         }
 
-        private void LearnStreamsInit(int CountOfStreams)
+        private void LearnStreamsInit(int CountOfStreams, int CountClassesOnStream)
         {
             LearnStreams = new LearningStream[CountOfStreams];
             for (int i = 0; i < this.LearnStreams.Length; i++)
             {
-                var learnStream = new LearningStream(4);
-                char classLetter = 'A';
-                var asciiCode = classLetter;
+                var learnStream = new LearningStream(CountClassesOnStream);
                 for (int j = 0; j < learnStream.Classes.Length; j++)
                 {
                     learnStream.Classes[j] = new StudyClass($"{i + 1}-{Char.ConvertFromUtf32(65 + j)}");
                 }
                 LearnStreams[i] = learnStream;
             }
+        }
+        private void TeachersInit(int NumberOfTeachers)
+        { 
+
         }
     }
 }
